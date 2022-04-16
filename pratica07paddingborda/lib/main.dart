3d4a7cfb22c17075add0804d3d20206e0c17254f
Alexandre Abreu Silva <47934004+1aLx@users.noreply.github.com>
2022-04-16 12:37:55 -0300

    Update main.dart

--- pratica07paddingborda/lib/main.dart
+++ pratica07paddingborda/lib/main.dart
@@ -9,7 +9,7 @@ void main() {
           textDirection: TextDirection.ltr,
         ),
         color: Colors.blue,
-        //Aplicando padding a todas as bordas co Container.
+        //Aplicando padding a todas as bordas do Container.
         padding: const EdgeInsets.all(15.0),
         width: 200.0,
         height: 100.0,
